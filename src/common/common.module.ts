328dbb8264679852ff6e32c50536d13654a28d54
dhardy <dhardy@iMac.local>
2017-11-14 11:51:53 -0500

    removed the commented out code for the common module that reference modules not used in this project

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,34 +1,19 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 
-
-// import { AppConfig } from './app.config';
-// import { AuthHttp } from './authHttp.service';
-// import { IDocument, IDocumentResult } from './document.inteface';
 import { throwIfAlreadyLoaded } from './module-import-guard';
-// import { i18nModule } from './i18n/i18n.module';
-// import { SecurityModule } from './security/security.module';
 import { CognitoModule } from '../cognito/cognito.module';
 import { ValidatorModule } from './validators/validator.module';
 
-// export * from './app.config';
-// export * from './document.inteface';
-// export * from '../common/user/user';
 export * from './module-import-guard';
-// export * from './security/security.config';
-// export * from './security/security.service';
 export * from '../cognito/cognito.service';
 export * from './validators/validator.service';
 
 @NgModule({
     imports: [
-        // i18nModule,
-        // SecurityModule,
         CognitoModule,
         ValidatorModule
     ],
     exports: [
-        // i18nModule,
-        // SecurityModule,
         CognitoModule,
         ValidatorModule
     ],
